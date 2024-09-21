975e28070ad6ac609569218ef690c41090200129
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-09-21 19:46:34 +0300

    Create viewset for book

--- books/views.py
+++ books/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from books.models import Book
+from books.serializers import BookSerializer
+
+
+class BookViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    GenericViewSet,
+):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.action == "list":
+            permission_classes = [AllowAny]
+        elif self.action != "list":
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
