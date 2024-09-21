587be426a2bc2ec3afb1079b3bce97747cb503ea
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-09-21 18:34:56 +0300

    Create borrowing model & register it in admin.py

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,15 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Borrowing(models.Model):
+    email = models.CharField(max_length=255)
+    expected_return = models.DateField()
+    actual_return = models.DateField(null=True, blank=True)
+    book_id = models.IntegerField(validators=[MinValueValidator(0)])
+    user_id = models.IntegerField(validators=[MinValueValidator(0)])
+
+    def is_late(self) -> bool:
+        if self.actual_return and self.actual_return > self.expected_return:
+            return True
+        return False
