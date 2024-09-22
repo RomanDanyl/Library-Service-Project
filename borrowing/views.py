5ae1c0359b4b94424d380b21ac1d680363fc088d
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-09-22 22:38:17 +0300

    Create views, urls for borrowing model

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from books.models import Book
+from borrowing.models import Borrowing
+from borrowing.serializers import BorrowingReadSerializer, BorrowingCreateSerializer
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated and self.request.user.is_staff:
+            return self.queryset
+        if self.request.user.is_authenticated:
+            return self.queryset.filter(user_id=self.request.user.id)
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return BorrowingReadSerializer
+        if self.action == "create":
+            return BorrowingCreateSerializer
+        return BorrowingReadSerializer
