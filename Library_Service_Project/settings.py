ec1e6c00ff59c0813954f703ed58c70e2c87a13d
RomanDanyl <162486585+RomanDanyl@users.noreply.github.com>
2024-09-25 23:31:00 +0300

    Add django SECRET_KEY to .env

--- Library_Service_Project/settings.py
+++ Library_Service_Project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-gruts)b$^6)#gk%j1uu$z*mej099y-(%u5na@0aza6uo*sy8#2"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
